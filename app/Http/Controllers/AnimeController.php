0a0d858fac33b8c59daca04337bb4cba81c5742a
Roberto Samudio <robe.arch@gmail.com>
2021-07-17 02:29:05 -0500

    Anime API CRUD

---
+++ app/Http/Controllers/AnimeController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Anime;
+use Illuminate\Http\Request;
+
+class AnimeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Anime::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name'=>'required',
+            'slug'=>'required',
+            'sinopsis'=>'required'
+        ]);
+
+        return Anime::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Anime::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $anime=Anime::find($id);
+        $anime->update($request->all());
+        return $anime;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Anime::destroy($id);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AnimeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/anime', [AnimeController::class, 'index']);
+Route::get('/anime/{id}', [AnimeController::class, 'show']);
+Route::post('/anime', [AnimeController::class, 'store']);
+Route::put('/anime/{id}', [AnimeController::class, 'update']);
+Route::delete('/anime/{id}', [AnimeController::class, 'destroy']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
